71d35b898de1606849df5fd216d712878b3996a6
Benoit KUGLER <x.ben.x@free.fr>
2019-09-23 22:48:51 +0200

    enforce check of special cases

--- style/properties/main.go
+++ style/properties/main.go
@@ -21,8 +21,31 @@ type CssProperty interface {
 // or one the 3 special values var(), attr() or custom properties
 // "initial" and "inherited" values have been resolved
 type CascadedProperty struct {
-	CssProperty
+	prop            CssProperty
 	SpecialProperty specialProperty
+}
+
+// AsCss will panic if c.SpecialProperty is not nil.
+func (c CascadedProperty) AsCss() CssProperty {
+	if c.SpecialProperty != nil {
+		panic("attempted to bypass the SpecialProperty of a CascadedProperty")
+	}
+	return c.prop
+}
+
+// ValidatedProperty is valid css input, so it may contain
+// a classic property, a special one, or one of the keyword "inherited" or "initial".
+type ValidatedProperty struct {
+	prop    CascadedProperty
+	Default defaultKind
+}
+
+// AsCascaded will panic if c.Default is not zero.
+func (c ValidatedProperty) AsCascaded() CascadedProperty {
+	if c.Default != 0 {
+		panic("attempted to bypass the Default of a ValidatedProperty")
+	}
+	return c.prop
 }
 
 type specialProperty interface {
@@ -31,11 +54,8 @@ type specialProperty interface {
 
 type defaultKind uint8
 
-// ValidatedProperty is valid css input, so it may contain
-// a classic property, a special one, or one of the keyword "inherited" or "initial".
-type ValidatedProperty struct {
-	CascadedProperty
-	Default defaultKind
+func (d defaultKind) ToV() ValidatedProperty {
+	return ValidatedProperty{Default: d}
 }
 
 type VarData struct {
@@ -57,11 +77,11 @@ func (v CustomProperty) isSpecialProperty() {}
 // Note than a CssProperty can naturally be seen as a CascadedProperty, but not the other way around.
 
 func ToC(prop CssProperty) CascadedProperty {
-	return CascadedProperty{CssProperty: prop}
+	return CascadedProperty{prop: prop}
 }
 
 func (c CascadedProperty) ToV() ValidatedProperty {
-	return ValidatedProperty{CascadedProperty: c}
+	return ValidatedProperty{prop: c}
 }
 
 // Properties is the general container for validated, cascaded and computed properties.
