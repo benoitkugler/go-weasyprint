5760ec740810634a626e161b20f7464653ea6e59
Benoit KUGLER <x.ben.x@free.fr>
2022-01-13 19:39:18 +0100

    add test for basic patterns

--- pdf/draw_svg_test.go
+++ pdf/draw_svg_test.go
@@ -37,3 +37,141 @@ func TestUse(t *testing.T) {
       </svg>
     `)
 }
+
+// Test how SVG simple patterns are drawn.
+
+func TestPattern(t *testing.T) {
+	capt := testutils.CaptureLogs()
+	defer capt.AssertNoLogs(t)
+
+	assertPixelsEqual(t, "pattern", `
+        BBrrBBrr
+        BBrrBBrr
+        rrBBrrBB
+        rrBBrrBB
+        BBrrBBrr
+        BBrrBBrr
+        rrBBrrBB
+        rrBBrrBB
+    `, `
+      <style>
+        @page { size: 8px }
+        svg { display: block }
+      </style>
+      <svg width="8px" height="8px" xmlns="http://www.w3.org/2000/svg">
+        <defs>
+          <pattern id="pat" x="0" y="0" width="4" height="4"
+            patternUnits="userSpaceOnUse"
+            patternContentUnits="userSpaceOnUse">
+            <rect x="0" y="0" width="2" height="2" fill="blue" />
+            <rect x="0" y="2" width="2" height="2" fill="red" />
+            <rect x="2" y="0" width="2" height="2" fill="red" />
+            <rect x="2" y="2" width="2" height="2" fill="blue" />
+          </pattern>
+        </defs>
+        <rect x="0" y="0" width="8" height="8" fill="url(#pat)" />
+      </svg>
+    `)
+}
+
+func TestPattern_2(t *testing.T) {
+	capt := testutils.CaptureLogs()
+	defer capt.AssertNoLogs(t)
+
+	assertPixelsEqual(t, "pattern_2", `
+        BBrrBBrr
+        BBrrBBrr
+        rrBBrrBB
+        rrBBrrBB
+        BBrrBBrr
+        BBrrBBrr
+        rrBBrrBB
+        rrBBrrBB
+    `, `
+      <style>
+        @page { size: 8px }
+        svg { display: block }
+      </style>
+      <svg width="8px" height="8px" xmlns="http://www.w3.org/2000/svg">
+        <defs>
+          <pattern id="pat" x="0" y="0" width="50%" height="50%"
+            patternUnits="objectBoundingBox"
+            patternContentUnits="userSpaceOnUse">
+            <rect x="0" y="0" width="2" height="2" fill="blue" />
+            <rect x="0" y="2" width="2" height="2" fill="red" />
+            <rect x="2" y="0" width="2" height="2" fill="red" />
+            <rect x="2" y="2" width="2" height="2" fill="blue" />
+          </pattern>
+        </defs>
+        <rect x="0" y="0" width="8" height="8" fill="url(#pat)" />
+      </svg>
+    `)
+}
+
+func TestPattern_3(t *testing.T) {
+	capt := testutils.CaptureLogs()
+	defer capt.AssertNoLogs(t)
+
+	assertPixelsEqual(t, "pattern_3", `
+        BBrrBBrr
+        BBrrBBrr
+        rrBBrrBB
+        rrBBrrBB
+        BBrrBBrr
+        BBrrBBrr
+        rrBBrrBB
+        rrBBrrBB
+    `, `
+      <style>
+        @page { size: 8px }
+        svg { display: block }
+      </style>
+      <svg width="8px" height="8px" xmlns="http://www.w3.org/2000/svg">
+        <defs>
+          <pattern id="pat" x="0" y="0" width="4" height="4"
+            patternUnits="userSpaceOnUse"
+            patternContentUnits="userSpaceOnUse">
+            <rect x="0" y="0" width="2" height="2" fill="blue" />
+            <rect x="0" y="2" width="2" height="2" fill="red" />
+            <rect x="2" y="0" width="2" height="2" fill="red" />
+            <rect x="2" y="2" width="2" height="2" fill="blue" />
+          </pattern>
+        </defs>
+        <rect x="0" y="0" width="8" height="8" fill="url(#pat)" />
+      </svg>
+    `)
+}
+
+func TestPattern_4(t *testing.T) {
+	capt := testutils.CaptureLogs()
+	defer capt.AssertNoLogs(t)
+
+	assertPixelsEqual(t, "pattern_4", `
+        BBrrBBrr
+        BBrrBBrr
+        rrBBrrBB
+        rrBBrrBB
+        BBrrBBrr
+        BBrrBBrr
+        rrBBrrBB
+        rrBBrrBB
+    `, `
+      <style>
+        @page { size: 8px }
+        svg { display: block }
+      </style>
+      <svg width="8px" height="8px" xmlns="http://www.w3.org/2000/svg">
+        <defs>
+          <pattern id="pat" x="0" y="0" width="4" height="4"
+            patternUnits="userSpaceOnUse"
+            patternContentUnits="objectBoundingBox">
+            <rect x="0" y="0" width="50%" height="50%" fill="blue" />
+            <rect x="0" y="50%" width="50%" height="50%" fill="red" />
+            <rect x="50%" y="0" width="50%" height="50%" fill="red" />
+            <rect x="50%" y="50%" width="50%" height="50%" fill="blue" />
+          </pattern>
+        </defs>
+        <rect x="0" y="0" width="8" height="8" fill="url(#pat)" />
+      </svg>
+    `)
+}
